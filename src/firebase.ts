3c7e377334160118e5f0b0c88023eff5b5c7426e
@RacoonsInATrenchcoat <tabitamas91@gmail.com>
2026-06-21 12:18:54 +0100

    small change to firebase.ts, so Vite doesnt scream error with an unused function.

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { getAnalytics, logEvent } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -19,3 +19,5 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+//logevents: FB analytics on how many times the site was loaded. Also pulls analytics to be used, so Vite doesnt error out due to unused func.
+logEvent(analytics, "app_loaded");
